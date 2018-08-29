46a5c2a9a0319e622d38a3ef904d39c67f841cc7
tomytomyk <to6-mi25.ta43@ezweb.ne.jp>
2018-08-29 12:35:59 +0000

    Finish 5-8

---
+++ app/models/post_image.rb
@@ -0,0 +1,15 @@
+class PostImage < ApplicationRecord
+
+	belongs_to :user
+	has_many :favorites, dependent: :destroy
+	has_many :post_comments, dependent: :destroy
+
+	validates :shop_name, presence: true
+    validates :image, presence: true
+
+	attachment :image
+
+	def favorited_by?(user)
+        favorites.where(user_id: user.id).exists?
+    end
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'fc268c0e562714086bcba059ca8ddbb0e36a9e847a6197299b97328889ce876ded0c3c8d6d29f2108e5703e2b6de9cfd65eb719416baf5b35f05d7455424b573'
